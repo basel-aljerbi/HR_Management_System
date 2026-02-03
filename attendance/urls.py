92cb943cfcc525ee2d36ead3fc8c169e8b6201c7
basel-aljerbi <baselaljerbi1@gmail.com>
2026-02-03 15:48:16 +0300

    feat: add attendance history endpoint

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import CheckInView, CheckOutView
+from .views import AttendanceHistoryView
 
 urlpatterns = [
     path('attendance/check-in/', CheckInView.as_view()),
     path('attendance/check-out/', CheckOutView.as_view()),
+     path('attendance/history/', AttendanceHistoryView.as_view()),
 ]
